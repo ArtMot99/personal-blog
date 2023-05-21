5ac82f97419819cc22de6143c46637fe1fff46e5
Artem Motrunich <motrunich2013@gmail.com>
2023-05-21 23:00:01 +0200

    Add a post model that is ordered by the created_at field

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -16,7 +17,33 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ['name']
 
     def str(self) -> str:
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    categories = models.ManyToManyField(
+        Category,
+        related_name='posts'
+    )
+    image = models.ImageField(
+        upload_to='post_images',
+        default='',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def str(self) -> str:
+        return self.title

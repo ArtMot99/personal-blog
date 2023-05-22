ae3d01809935d4817ab503b691b832f9b8e59e16
Artem Motrunich <motrunich2013@gmail.com>
2023-05-22 12:31:20 +0200

    Register category model with search field by name

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from blog.models import User
+from blog.models import User, Category
 
 
 @admin.register(User)
@@ -8,3 +8,9 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional info", {"fields": ("first_name", "last_name")}),
     )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]

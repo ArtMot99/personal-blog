72d0834b0ef7e313bdfd5971c703b9be4fe5a382
Artem Motrunich <motrunich2013@gmail.com>
2023-05-22 13:45:52 +0200

    Add the index view for the home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.db.models import Count
+from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def index(request) -> HttpResponse:
+    """Index view for the blog project home page"""
+    posts = Post.objects.select_related("author", "categories").annotate(
+        num_comments=Count("comments")
+    )
+    context = {"posts": posts}
+
+    return render(request, "blog/index.html", context=context)

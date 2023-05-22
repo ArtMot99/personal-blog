228d1eb260c731631224ab671ce539889cf51f6e
Artem Motrunich <motrunich2013@gmail.com>
2023-05-22 12:19:10 +0200

    Register user model for management from the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from blog.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name")}),
+    )

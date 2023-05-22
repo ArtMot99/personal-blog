820b62d3bb17a4533de2777692e662f10a6f4929
Artem Motrunich <motrunich2013@gmail.com>
2023-05-22 15:28:31 +0200

    Add pagination option to index view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -6,11 +7,14 @@ from blog.models import Post
 
 def index(request) -> HttpResponse:
     """Index view for the blog project home page"""
-    posts = (
+    post_list = (
         Post.objects.select_related("author")
         .prefetch_related("categories")
         .annotate(num_comments=Count("comments"))
     )
-    context = {"posts": posts}
+    paginator = Paginator(post_list, 4)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {"page_obj": page_obj}
 
     return render(request, "blog/index.html", context=context)

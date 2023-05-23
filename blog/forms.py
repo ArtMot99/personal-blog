7829453b5dc9df8796b157bd050bd5964f7a82f2
Artem Motrunich <motrunich2013@gmail.com>
2023-05-23 13:41:39 +0200

    Add a form for leaving a comment on each post page

--- blog/forms.py
+++ blog/forms.py
@@ -20,3 +20,13 @@ class PostSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by keywords..."}),
     )
+
+
+class CommentForm(forms.Form):
+    text = forms.CharField(
+        max_length=255,
+        label="",
+        widget=forms.Textarea(
+            attrs={"placeholder": "Join the discussion and leave a comment!"}
+        ),
+    )

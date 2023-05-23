697fc25ab4f37b8fec512fd7e0c4f1fd80bfd55c
Artem Motrunich <motrunich2013@gmail.com>
2023-05-23 13:38:49 +0200

    Add a path to go to the detailed display of each post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, PostDetailView
 
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]

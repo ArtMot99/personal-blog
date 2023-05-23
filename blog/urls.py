36099108f15291512185d5946a1e411ab73ffc41
Artem Motrunich <motrunich2013@gmail.com>
2023-05-23 17:42:13 +0200

    Add a new path for user registration

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from blog.views import index, PostDetailView
-
+from blog.views import index, PostDetailView, register
 
 app_name = "blog"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("signup/", register, name="sign-up"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]

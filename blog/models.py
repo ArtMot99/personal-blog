cd6828dec2f827364c388a0c1735c02d27b50cca
Artem Motrunich <motrunich2013@gmail.com>
2023-05-26 13:23:31 +0200

    Add new ContactMessage model for feedback

--- blog/models.py
+++ blog/models.py
@@ -53,3 +53,17 @@ class Comment(models.Model):
 
     def __str__(self) -> str:
         return self.text
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=63)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name

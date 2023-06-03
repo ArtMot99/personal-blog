f6feae9a84f82f1de850aba34d5f6a62a1052d82
Artem Motrunich <motrunich2013@gmail.com>
2023-06-03 13:21:26 +0200

    Add manage staticfiles to urls.py

--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("blog.urls", namespace="blog")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
